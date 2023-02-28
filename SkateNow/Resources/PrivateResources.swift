97b33023ced3515cf2620e23d0e59624b3650717
Egor Nesterenko <nesterenkoegorbussines@gmail.com>
2023-02-28 18:42:54 +0300

    fix gitignore 4

--- SkateNow/Resources/PrivateResources.swift
+++
@@ -1,8 +0,0 @@
-enum PrivateResources {
-    static let storageURL = "gs://skatenow-92308.appspot.com"
-    static let usersCollection = "users"
-    static let usersTransportKey = "transport"
-    static let usersDoneTricksKey = "doneTricks"
-    static let usersNameKey = "name"
-    static let usersVisitedSpotsKey = "visitedSpots"
-}
